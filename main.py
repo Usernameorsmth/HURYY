7d1950ec62ee6a149d6bf8b670759fff1804259e
Usernameorsmth <simidarawilliams@gmial.com>
2025-06-11 18:45:59 +0100

    ugh

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-#Create an empty list
-empty_list = []
-print(empty_list)
+# function to check whether
+# first and last character of words match
+def match_words(words):
+    ctr = 0
+    lst = []
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            ctr += 1
+            lst.append(word)
 
-# A list of numbers
-numbers = [1, 2, 3, 4, 5]
-print(numbers)
+    print("List of words with first and last character same\n", lst)
+    return ctr
 
-# Use * operator
-triples = [1, 2, 3] * 3
-print(triples)
-
-#reverse the given list
-aList = [100, 200, 300, 400, 500]
-aList = aList[::-1]
-print(aList, "\n")
+count = match_words(['abc', 'cfc', 'xyz', 'aba', '1221'])
+print("Number of words having first and last character same:", count)
