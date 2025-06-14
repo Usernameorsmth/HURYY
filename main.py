83cb7df406f545bc3d289fd399f3b90b1c92abb5
Usernameorsmth <simidarawilliams@gmial.com>
2025-06-14 19:24:25 +0100

    wouldntuliketoknowweatherboy

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-# function to check whether palindrome or not
-def palind(r):
-    e = len(r) - 1
-    s = 0
-    while(s<=e):
-        if(r[s]!=r[e]):
-            return False
-        s+=1
-        e-=1
-    return True
+weather=(1,0,0,0,1,1,0)
+sunny=0
+rainy=0
+for i in range(0,7):
+    if(weather[i]==0):
+        rainy+=1
+    else:
+        sunny+=1
 
-r = (1,2,3,3,3,2,1)
-
-if(palind(r)):
-    print("The Tuple is Flip-Flop")
+if(sunny>rainy):
+    print("Good weather")
 else:
-    print("The Tuple is not Flip-Flop")
+    print("Bad weather")
